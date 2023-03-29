fdf05bcf70e1d552993c64bf7e3d5c238481c696
bigscotia10 <chris.ferrell@pettygrovefull.com>
2023-03-29 14:35:03 -0700

    fixing it up

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// next.config.js
 
 module.exports = {
+  reactStrictMode: true,
   env: {
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
   },
-  nextConfig
 };
