0c0cfa62269b49e2db07a807b90e99ff6f40218d
bigscotia10 <chris.ferrell@pettygrovefull.com>
2023-03-29 15:10:18 -0700

    fix styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,63 +34,89 @@ export default function Home() {
       </form>
       {response && <p className="response">{response}</p>}
       <style jsx>{`
-        .container {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          justify-content: center;
-          height: 100vh;
-        }
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+  }
 
-        .form {
-          display: flex;
-          flex-direction: column;
-          border: 1px solid lightgray;
-          padding: 30px;
-          border-radius: 10px;
-          background-color: #f5f5f5;
-          width: 50%;
-        }
+  .form {
+    display: flex;
+    flex-direction: column;
+    border: 1px solid lightgray;
+    padding: 30px;
+    border-radius: 10px;
+    background-color: #f5f5f5;
+    width: 50%;
+  }
 
-        .label {
-          font-size: 24px;
-          margin-bottom: 10px;
-        }
+  .label {
+    font-size: 24px;
+    margin-bottom: 10px;
+  }
 
-        .input {
-          font-size: 24px;
-          padding: 10px;
-          border: 1px solid lightgray;
-          border-radius: 5px;
-          margin-bottom: 20px;
-          height: 150px;
-          width: 100%;
-        }
+  .input {
+    font-size: 24px;
+    padding: 10px;
+    border: 1px solid lightgray;
+    border-radius: 5px;
+    margin-bottom: 20px;
+    height: 150px;
+    width: 100%;
+  }
 
-        .button {
-          font-size: 24px;
-          padding: 10px;
-          border: 1px solid #4caf50;
-          border-radius: 5px;
-          background-color: #4caf50;
-          color: white;
-          cursor: pointer;
-        }
+  .button {
+    font-size: 24px;
+    padding: 10px;
+    border: 1px solid #4caf50;
+    border-radius: 5px;
+    background-color: #4caf50;
+    color: white;
+    cursor: pointer;
+  }
 
-        .button:hover {
-          background-color: #45a049;
-        }
+  .button:hover {
+    background-color: #45a049;
+  }
 
-        .response {
-          color: #4caf50;
-          font-size: 24px;
-          padding: 10px;
-          border-radius: 5px;
-          margin-bottom: 20px;
-          width: 50%;
-          text-align: center;
-        }
-      `}</style>
+  .response {
+    color: #4caf50;
+    font-size: 24px;
+    padding: 10px;
+    border-radius: 5px;
+    margin-bottom: 20px;
+    width: 50%;
+    text-align: center;
+  }
+
+  @media only screen and (max-width: 600px) {
+    .form {
+      width: 90%;
+      padding: 20px;
+    }
+
+    .label {
+      font-size: 20px;
+    }
+
+    .input {
+      font-size: 20px;
+      height: 100px;
+    }
+
+    .button {
+      font-size: 20px;
+      padding: 10px;
+    }
+
+    .response {
+      font-size: 20px;
+    }
+  }
+`}</style>
+
     </div>
   );
 }
